78b223e328dacdc784a2508a8b1ed1dadd4cbbd4
OakAndAspen <irinadespot@gmail.com>
2018-07-09 15:58:48 +0200

    Forum modal

--- src/CustomFunctions.js
+++ src/CustomFunctions.js
@@ -9,7 +9,37 @@ export default {
             } else return a[orderBy] - b[orderBy];
         });
 
-        if(!asc) return array.reverse();
+        if (!asc) return array.reverse();
         return array;
+    },
+    timeSince(date) {
+        var seconds = Math.floor((new Date() - date) / 1000);
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
     }
+
+    /*var aDay = 24*60*60*1000
+    console.log(timeSince(new Date(Date.now()-aDay)));
+    console.log(timeSince(new Date(Date.now()-aDay*2)));*/
+
 }
--- src/routes/private/Forum.js
+++ src/routes/private/Forum.js
@@ -8,10 +8,14 @@ export default class Forum extends React.Component {
         super(props);
         this.state = {
             'categories': [],
-            'topics': []
+            'topics': [],
+            'currentSel': null,
+            'currentTopic': null,
+            'modalOpen': false
         };
         this.render();
         this.handleCategoryChange = this.handleCategoryChange.bind(this);
+        this.showTopic = this.showTopic.bind(this);
     }
 
     componentDidMount() {
@@ -36,28 +40,59 @@ export default class Forum extends React.Component {
     }
 
     handleCategoryChange(e) {
-        let id = $(e.target).attr('data-id');
-        $('button').removeClass('active');
+        $('#Categories button').removeClass('active');
         $(e.target).addClass('active');
+        $(e.target).blur();
 
         if ($(e.target).attr('id') === "RecentTopics") {
-            $('.list-group-item').each(function (i) {
-                if (i<6) $(this).show();
-                else $(this).hide();
-            });
+            this.setState({currentSel: null});
         } else {
-            $('.list-group-item').each(function (i) {
-                let cat = $(this).attr("category");
-                if (id === cat) $(this).show();
-                else $(this).hide();
-            });
+            this.setState({currentSel: $(e.target).attr('data-id')});
         }
     }
 
+    showTopic(e) {
+        let id = $(e.target).attr("data-id");
+        $.ajax({
+            url: CF.apiUrl + '/topics/' + id,
+            context: this,
+        }).done(function (data) {
+            this.setState({
+                'topic': data
+            });
+            $('#TopicModal').modal();
+        });
+    }
+
     render() {
+        let modal = null;
+        if (this.state.topic) {
+            modal = <div className="modal" id="TopicModal" tabIndex="-1" role="dialog" aria-labelledby="TopicModalTitle"
+                         aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title"
+                                        id="TopicModalTitle">{this.state.topic.title}</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <h1>This is a test</h1>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        }
+
         return (
             <div className="row pt-3">
-                <div className="col-md-4">
+                <div className="col-md-4" id="Categories">
                     <button id="RecentTopics" className="btn TButton active w-100 my-1"
                             onClick={this.handleCategoryChange}>Sujets récents
                     </button>
@@ -69,10 +104,27 @@ export default class Forum extends React.Component {
                 </div>
                 <div className="col-md-8">
                     <ul className="list-group">
-                        {this.state.topics.map(function (t) {
-                            return <li className="list-group-item" key={t.id} category={t.category}>{t.title}</li>;
+                        {this.state.topics.map(function (t, i) {
+                            if ((this.state.currentSel == null && i < 6) || (parseInt(this.state.currentSel, 10) === parseInt(t.category, 10))) {
+                                return (
+                                    <li className="list-group-item d-flex" key={t.id} category={t.category}>
+                                        <div className="col-2">
+                                            <span className="badge badge-info mr-2">Non lu</span>
+                                        </div>
+                                        <div className="col-8">{t.title}</div>
+                                        <div className="col-2">
+                                            <button className="btn btn-info btn-sm" data-id={t.id}
+                                                    onClick={this.showTopic}>
+                                                Voir
+                                            </button>
+                                        </div>
+                                    </li>);
+                            } else return '';
                         }, this)}
                     </ul>
+                </div>
+                <div className="col-12">
+                    {modal}
                 </div>
             </div>
         );
